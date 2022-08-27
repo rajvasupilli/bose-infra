ebc253186720e03c6706d8a4b698092fff83a913
EC2 Default User <ec2-user@ip-172-31-20-116.ec2.internal>
2022-08-27 12:08:31 +0000

    initial draft

--- s3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bose-infra-tfstate"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
--- s3/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "bose-infra-tfstate"
-
-  tags = {
-    Name        = "Dev Terraform State Info"
-    Environment = "Dev"
-  }
-}
